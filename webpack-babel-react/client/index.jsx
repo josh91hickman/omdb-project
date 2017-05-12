7d9ab5d143ebf1031afbe4e481df002d389262ad
=josh91hickman <josh91hickman@gmail.com>
2017-05-12 10:42:50 -0700

    organize folder structure

--- webpack-babel-react/client/index.jsx
+++ webpack-babel-react/client/index.jsx
@@ -1,11 +1,14 @@
 import { Router, browserHistory } from 'react-router';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import store from './store';
+import store from './store/store';
 import routes from './routes';
+import './setup';
 
-export default (
+render(
   <Provider store={store}>
     <Router history={browserHistory}>{routes}</Router>
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
