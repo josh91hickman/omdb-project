61e5babfbe3cac3cadc230add83b10c2c77fbc2a
=josh91hickman <josh91hickman@gmail.com>
2017-05-12 10:24:56 -0700

    initial commit/setup

--- webpack-babel-react/ui/store.js
+++ webpack-babel-react/ui/store.js
@@ -1,12 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
+import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
-
 import reducer from './reducer';
 import saga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware, thunk];
 
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
+const store = createStore(reducer, applyMiddleware(...middlewares));
 
 sagaMiddleware.run(saga);
 
